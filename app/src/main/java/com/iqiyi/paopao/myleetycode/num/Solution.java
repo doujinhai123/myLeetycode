211349f9cfda4d873d14abebf240511224ec4e62
doujinhai <doujinhai@bigo.sg>
2020-08-25 20:45:13 +0800

    sss

--- app/src/main/java/com/iqiyi/paopao/myleetycode/num/Solution.java
+++ app/src/main/java/com/iqiyi/paopao/myleetycode/num/Solution.java
@@ -285,6 +285,20 @@ public int[] twoSumss(int[] nums, int target) {
         return -1;
 
     }
+    //把数组排成最小的数
+    public String minNumber(int[] nums) {
+        String[] strs = new String[nums.length];
+        for(int i = 0; i < nums.length; ++i)
+            strs[i] = String.valueOf(nums[i]);
+        Arrays.sort(strs,(o1,o2) -> {
+            return (o1+o2).compareTo(o2+o1);
+        });
+        StringBuilder sb = new StringBuilder();
+        for(String s: strs)
+            sb.append(s);
+        return sb.toString();
+    }
+
 
 
 }
