6bff23071f09d44802f50b6f52233eb4365b4634
doujinhai <doujinhai@bigo.sg>
2020-09-03 17:04:30 +0800

    sssssss

--- app/src/main/java/com/iqiyi/paopao/myleetycode/stack/Solution.java
+++ app/src/main/java/com/iqiyi/paopao/myleetycode/stack/Solution.java
@@ -6,8 +6,8 @@ public class Solution {
     Stack<Integer> stackPush = new Stack<Integer>();
     Stack<Integer> stackPop = new Stack<Integer>();
 
-    public void push(int node) {
-        stackPush.push(node);
+    public void push(int x) {
+        stackPush.push(x);
     }
 
     public int pop() {
@@ -19,5 +19,11 @@ public class Solution {
             }
             return  stackPop.pop();
         }
+    }
+    public boolean empty() {
+        return stackPop.isEmpty()&& stackPush.isEmpty();
+    }
+    public int peek() {
+        return  stackPop.peek();
     }
 }
---
+++ app/src/main/java/com/iqiyi/paopao/myleetycode/stack/minSolution.java
@@ -0,0 +1,36 @@
+package com.iqiyi.paopao.myleetycode.stack;
+
+import java.util.Stack;
+
+class MinStack {
+    Stack<Integer> stackA = new Stack<>();
+    Stack<Integer> stackB = new Stack<>();
+    /** initialize your data structure here. */
+    public MinStack() {
+
+    }
+
+    public void push(int node) {
+        stackA.push(node);
+        if (stackB.isEmpty()|| stackB.peek() >= node) {
+            stackB.push(node);
+        }
+
+    }
+
+    public void pop() {
+
+        if(stackA.pop().equals(stackB.peek())) {
+            stackB.pop();
+        }
+
+    }
+
+    public int top() {
+        return stackA.peek();
+    }
+
+    public int getMin() {
+        return stackB.peek();
+    }
+}
