fa81f8b908182b983ec58007525a2d4cedd31226
doujinhai <doujinhai@bigo.sg>
2020-09-15 12:19:42 +0800

    ssss

--- app/src/main/java/com/iqiyi/paopao/myleetycode/tree/Solution.java
+++ app/src/main/java/com/iqiyi/paopao/myleetycode/tree/Solution.java
@@ -76,14 +76,36 @@ class Solution {
             reslt[i] = arrayList.get(i);
         }
         return reslt;
+    }
 
 
-
-
-
-
+    //二叉树的层序遍历
+    public List<List<Integer>> levelOrderww(TreeNode root) {
+        List<List<Integer>>  result = new ArrayList<>();
+        if(root == null) {
+            return  result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left != null) {
+                    queue.add(node.left);
+                }
+                if(node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            result.add(list);
+        }
+        return result;
 
     }
+
 
 
 }
