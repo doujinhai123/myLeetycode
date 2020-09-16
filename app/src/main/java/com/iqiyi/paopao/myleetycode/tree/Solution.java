e0ad8986bfa8e8ee96258915f25d93977ea800f0
doujinhai <doujinhai@bigo.sg>
2020-09-16 10:00:42 +0800

    ssssss

--- app/src/main/java/com/iqiyi/paopao/myleetycode/tree/Solution.java
+++ app/src/main/java/com/iqiyi/paopao/myleetycode/tree/Solution.java
@@ -6,10 +6,11 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 class Solution {
    ArrayList arrayList = new ArrayList();
-   public void main(String[] args) {
+   public static void main(String[] args) {
        TreeNode treeNode = new TreeNode(0);
        treeNode.left = new TreeNode(1);
        treeNode.right = new TreeNode(2);
@@ -17,6 +18,7 @@ class Solution {
        treeNode.left.right = null;
        treeNode.right.left = new TreeNode(3);
        treeNode.right.right = new TreeNode(4);
+       levelOrderwwzhi(treeNode);
 
    }
    //二叉树的前学
@@ -40,17 +42,17 @@ class Solution {
         return arrayList;
 
     }
-    //二叉树的后序遍历
-    public List<Integer> inorderTraversal(TreeNode root) {
-        if(root == null) {
-            return arrayList;
-        }
-        inorderTraversal(root.left);
-        inorderTraversal(root.right);
-        arrayList.add(root.val);
-        return arrayList;
-
-    }
+//    //二叉树的后序遍历
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        if(root == null) {
+//            return arrayList;
+//        }
+//        inorderTraversal(root.left);
+//        inorderTraversal(root.right);
+//        arrayList.add(root.val);
+//        return arrayList;
+//
+//    }
     //二叉树的层序遍历
     public int[] levelOrder(TreeNode root) {
         ArrayList<Integer> arrayList = new ArrayList();
@@ -103,6 +105,37 @@ class Solution {
             result.add(list);
         }
         return result;
+
+    }
+
+
+    //二叉树的之字形打印
+    public static void levelOrderwwzhi(TreeNode root) {
+        Stack<TreeNode> stack1 = new Stack<>();
+        stack1.push(root);
+        Stack<TreeNode> stack2 = new Stack<>();
+        while (!stack1.isEmpty()||!stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                TreeNode treeNode = stack1.pop();
+                System.out.println(treeNode.val);
+                if(treeNode.right != null) {
+                    stack2.push(treeNode.left);
+                }
+                if(treeNode.left != null) {
+                    stack2.push(treeNode.right);
+                }
+            }
+            while (!stack2.isEmpty()) {
+                TreeNode treeNode = stack2.pop();
+                System.out.println(treeNode.val);
+                if(treeNode.right != null) {
+                    stack1.push(treeNode.right);
+                }
+                if(treeNode.left != null) {
+                    stack1.push(treeNode.left);
+                }
+            }
+        }
 
     }
 
