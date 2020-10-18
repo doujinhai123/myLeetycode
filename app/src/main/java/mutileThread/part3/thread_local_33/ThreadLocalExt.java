ed7b25efbbb106b0db10bee3cf63ead75b0bc7b5
doujinhai <doujinhai@bigo.sg>
2020-10-18 16:30:14 +0800

    ssss

---
+++ app/src/main/java/mutileThread/part3/thread_local_33/ThreadLocalExt.java
@@ -0,0 +1,16 @@
+package mutileThread.part3.thread_local_33;
+
+/**
+ * className ThreadLocalExt
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午2:19
+ */
+public class ThreadLocalExt extends ThreadLocal<Long> {
+    @Override
+    protected Long initialValue() {
+        return System.currentTimeMillis();
+    }
+}
