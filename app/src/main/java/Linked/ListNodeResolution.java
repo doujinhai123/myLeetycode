4f4adcf89cd373499670080f4cd69d99fb97df50
doujinhai <doujinhai@bigo.sg>
2021-01-31 19:58:30 +0800

    sss

--- app/src/main/java/Linked/ListNodeResolution.java
+++ app/src/main/java/Linked/ListNodeResolution.java
@@ -318,6 +318,24 @@ public class ListNodeResolution {
         }
     }
 
+    //链表相加
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode prev = head;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;//求两个节点相加的值
+            carry = sum / 10;//取进位的值
+            prev.next = new ListNode(sum % 10);//sum对10求余后放到节点中
+            prev = prev.next;
+            l1 = l1 != null ? l1.next : l1;
+            l2 = l2 != null ? l2.next : l2;
+        }
+        return head.next;
+    }
+
+
 
 
     public static void main(String[] args) {
@@ -345,6 +363,8 @@ public class ListNodeResolution {
         isPalindrome(listNode);
         //合并有序链表
         mergeTwoLists(listNode,listNode);
+        //链表相加
+        addTwoNumbers(listNode,listNode);
     }
 
 
