64d917ab59e49b855f5f5eda97067bd467257037
doujinhai <doujinhai@bigo.sg>
2021-02-05 18:08:59 +0800

    ssss

--- app/src/main/java/ThreadAlgorithm/printerJiou.java
+++ app/src/main/java/ThreadAlgorithm/printerJiou.java
@@ -6,57 +6,100 @@ import java.util.concurrent.locks.ReentrantLock;
 public class printerJiou {
     static boolean flag = false;
     public static void main(String[] args) {
+        //方式1 通过reentrantLock
         ReentrantLock reentrantLock = new ReentrantLock();
         Condition conditionA = reentrantLock.newCondition();
         Condition conditionB = reentrantLock.newCondition();
-        Object object = new Object();
+
         new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i <100 ; i++) {
+                while (true) {
                     reentrantLock.lock();
                     try {
-                        Thread.sleep(1000);
-                        if(i%2 == 0) {
-                            System.out.println("打印的是偶数---------" + i);
-                            conditionB.signalAll();
-                            conditionA.await();
+                        for (int i = 0; i < 100; i++) {
+                            if (i % 2 == 0) {
+                                System.out.println("打印的为偶数" + i);
+                                conditionB.signalAll();
+                                conditionA.await();
+                            }
+
                         }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
-                    }finally {
+                    } finally {
+                        reentrantLock.unlock();
+                    }
+                }
+
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    reentrantLock.lock();
+                    try {
+                        for (int i = 0; i < 100; i++) {
+                            if (i % 2 == 1) {
+                                System.out.println("打印的为奇数" + i);
+                                conditionA.signalAll();
+                                conditionB.await();
+                            }
+                        }
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
                         reentrantLock.unlock();
                     }
                 }
             }
         }).start();
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
+//        //方式2通过对象锁
+//        Object object = new Object();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                synchronized (object) {
+//                    for (int i = 0; i < 100; i++) {
+//                        try {
+//                            Thread.sleep(2000);
+//                            if (i % 2 == 0) {
+//                                System.out.println("打印偶数线程为i----" + i);
+//                                object.notifyAll();
+//                                object.wait();
+//                            }
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }
+//            }
+//        }).start();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                synchronized (object) {
+//                    for (int i = 0; i < 100; i++) {
+//                        try {
+//                            Thread.sleep(2000);
+//                            if (i % 2 == 1) {
+//                                System.out.println("打印奇数线程为i----" + i);
+//                                object.notifyAll();
+//                                object.wait();
+//                            }
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }
+//            }
+//        }).start();
+//    }
 
-                for (int i = 0; i <100 ; i++) {
-                    reentrantLock.lock();
-                    try {
-                        Thread.sleep(1000);
-                        if(i%2 == 1) {
-                            System.out.println("打印的是ji数---------" + i);
-                            conditionA.signalAll();
-                            conditionB.await();
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }finally {
-                        reentrantLock.unlock();
-                    }
-                }
-            }
-        }).start();
 
     }
 }
